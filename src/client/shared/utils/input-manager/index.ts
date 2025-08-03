94cd33e3404ade4f7b377e26f1024645c3ecfb22
Brandon Kong <95515854+brandon-kong@users.noreply.github.com>
2025-08-03 18:53:10 -0500

    feat: implemented input manager

--- src/client/shared/utils/input-manager/index.ts
+++ src/client/shared/utils/input-manager/index.ts
@@ -1,16 +1,63 @@
+import { UserInputService } from '@rbxts/services';
+
 export type InputContext = string;
 
+export interface InputActionBinding {
+    name: string;
+
+    keys?: Enum.KeyCode[];
+    inputTypes?: Enum.UserInputType[];
+
+    callback: (inputObject: InputObject, gameProcessed: boolean) => void;
+
+    context?: InputContext;
+    consumeInput?: boolean;
+}
+
 export class InputManager {
-    private static instance: InputManager | undefined;
+    private readonly bindings: Map<string, InputActionBinding[]> = new Map();
+    private static readonly instance: InputManager = new InputManager();
+    private currentContext: string = 'Game';
 
     private constructor() {
-        // Logic here
+        UserInputService.InputBegan.Connect((input, gameProcessed) => {
+            if (gameProcessed) return;
+
+            for (const [_, bindings] of this.bindings) {
+                for (const binding of bindings) {
+                    const matchesKey = binding.keys && binding.keys.includes(input.KeyCode);
+                    const matchesInputType = binding.inputTypes && binding.inputTypes.includes(input.UserInputType);
+                    const matchesContext = binding.context === undefined || binding.context === this.currentContext;
+
+                    if (matchesContext && (matchesKey || matchesInputType)) {
+                        binding.callback(input, gameProcessed);
+                        if (binding.consumeInput) return;
+                    }
+                }
+            }
+        });
     }
 
     public static getInstance(): InputManager {
-        if (!this.instance) {
-            this.instance = new InputManager();
-        }
         return this.instance;
+    }
+
+    public bindAction(binding: InputActionBinding): void {
+        if (!this.bindings.has(binding.name)) {
+            this.bindings.set(binding.name, []);
+        }
+        this.bindings.get(binding.name)?.push(binding);
+    }
+
+    public unbindAction(bindingName: string): void {
+        this.bindings.delete(bindingName);
+    }
+
+    public setContext(context: string): void {
+        this.currentContext = context;
+    }
+
+    public clearAll(): void {
+        this.bindings.clear();
     }
 }
